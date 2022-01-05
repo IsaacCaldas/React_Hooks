00e768708274cedf8e4836158f303a885f6355fb
Isaac_Caldas <isaaccaldasgoncalves@gmail.com>
2022-01-05 17:43:31 -0300

    Hook: useRef()

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -2,14 +2,30 @@ import React, { useRef, useState, useEffect } from 'react';
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle';
 
+const merge = function(char1, char2){
+	return char1 + char2;
+}
+
 const UseRef = (props) => {
 
 	const [value1, setValue1] = useState('');
+	const [value2, setValue2] = useState('');
+
 	const count = useRef(0);
-	
+	const myInput1 = useRef(null);
+	const myInput2 = useRef(null);
+
+	console.log(myInput1.current);
+	console.log(myInput2.current);
+
 	useEffect(function() {
-		count.current = count.current + 1;
+		count.current++;
+		myInput2.current.focus();
 	}, [value1]);
+	useEffect(function() {
+		count.current++;
+		myInput1.current.focus();
+	}, [value2]);
 
 	return (
 		<div className="UseRef">
@@ -21,15 +37,21 @@ const UseRef = (props) => {
 				<div className="center">
 					<div>
 						<span className="text">Valor: </span>
-						<span className="text">{value1} [</span>
+						<span className="text">{merge(value1, value2)} [</span>
 						<span className="text red">{count.current}</span>
 						<span className="text">]</span>
 					</div>
 					<input type="text" className="input" 
-						value={value1} onChange={e => setValue1(e.target.value)} />
+						ref={myInput1}
+						value={value1} onChange={e => setValue1(e.target.value) } />
 				</div>
 
 			<SectionTitle title="Exercise useRef - 02" />
+			<div className="center">
+				<input type="text" className="input" 
+					ref={myInput2}
+					value={value2} onChange={e => setValue2(e.target.value) } />
+			</div>
 		</div>
 	)
 }
